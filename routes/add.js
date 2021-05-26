5952357b1b394e0786d3f8194989649126f62f85
BenbenYao1206 <2220624185@qq.com>
2021-05-26 09:26:18 +0800

    增删改查

--- routes/add.js
+++ routes/add.js
@@ -1,43 +1,43 @@
-// let express = require ('express');
-// let router = express.Router();
+let express = require ('express');
+let router = express.Router();
 
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "123456",
-//     database: "project"
-// });
-// connection.connect();
-
-// // router.get('/', function (req, res) {
-// //     res.render('add');
-// // });
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "project"
+});
+connection.connect();
 
 // router.get('/', function (req, res) {
-//     var selectSQL = "select * from pro_manager " 
-//       connection.query(selectSQL, function (err, results, fields){
-//         console.log(err);
-//         console.log(results);
-//         console.log(fields);
-//         res.render('add',{detail:results} );
-//       
-//         });
-//   });
+//     res.render('add');
+// });
 
-// router.post('/', (req, res) => {
+router.get('/', function (req, res) {
+    var selectSQL = "select * from pro_manager " 
+      connection.query(selectSQL, function (err, results, fields){
+        console.log(err);
+        console.log(results);
+        console.log(fields);
+        res.render('add',{detail:results} );
+      
+        });
+  });
 
-//     var insertSql = 'insert into pro_manager(name,phone,juice,shop) values(?,?,?,?)';
-//     connection.query(insertSql, [req.body.name,req.body.phone,req.body.juice,req.body.shop], function (err, result, fields) {
+router.post('/', (req, res) => {
+
+    var insertSql = 'insert into pro_manager(name,phone,juice,shop) values(?,?,?,?)';
+    connection.query(insertSql, [req.body.name,req.body.phone,req.body.juice,req.body.shop], function (err, result, fields) {
     
-//         if (err) {
-//             console.log('err', err);
-//             return;
-//         } else {
+        if (err) {
+            console.log('err', err);
+            return;
+        } else {
            
-//             res.redirect('/manager');
-//         }
-//     });
-//     });
+            res.redirect('/manager');
+        }
+    });
+    });
 
-// module.exports = router;
+module.exports = router;
