4cd202ba2096f285b3a27457029b550a83f73bee
BenbenYao1206 <2220624185@qq.com>
2021-05-31 11:48:19 +0800

    购物车

--- routes/juice.js
+++ routes/juice.js
@@ -11,7 +11,7 @@ var connection =mysql.createConnection({
   
 })
 connection.connect();
-
+//数据库里上传到页面
 router.get('/', function (req, res, next) {
   var sql = 'select * from pro_juice';
   connection.query(sql, function (err, result, fields) {
@@ -21,6 +21,34 @@ router.get('/', function (req, res, next) {
       } else { res.render('juice', { data: result }); }
   });
 });
+
+//购物车
+router.get('/trolly', function (req, res) {
+  res.render('trolly')
+  // var selectSQL = "select * from pro_juice " 
+  //   connection.query(selectSQL, function (err, results, fields){
+  //     console.log(err);
+  //     console.log(results);
+  //     console.log(fields);
+  //     res.render('trolly',{detail:results} );
+  //   
+  //     });
+});
+
+router.post('/', (req, res) => {
+
+  var insertSql = 'insert into pro_trolly(name,introduce,money,amount,sum_money) values(?,?,?,?,?)';
+  connection.query(insertSql, [req.body.name,req.body.introduce,req.body.money,req.body.amount,req.body.sum_money], function (err, result, fields) {
+  
+      if (err) {
+          console.log('err', err);
+          return;
+      } else {
+         
+          res.redirect('/trolly');
+      }
+  });
+  });
 
 module.exports = router;
 
